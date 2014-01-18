304417de7982515953cd12884557b22a15bb1e00
Fernando Paolo <fspaolo@gmail.com>
2014-01-18 12:50:40 -0800

    updated plot_matrix

--- altimpy/scripts/stack_files.py
+++ altimpy/scripts/stack_files.py
@@ -9,12 +9,21 @@ Notes
 -----
 - center (mean = 0) and normalize (std = 1) each time series.
 - for (2), window each time series to assure continuity in concatenation. 
-- (1) is suited for MSSA and (2) is suited for MTM.
+- (1) is suited for MSSA[1] and (2) is suited for MTM/MEM[2].
+
+[1] multi-channel singular spectrum analysis
+[2] multi-taper/maximum entropy methods
+
+Selecting from ParaView
+-----------------------
+Select Cells With Polygon > Plot Selection Over Time > Save Data
 
 Usage
 -----
 $ python stack_files.py *.csv
 
+Fernando Paolo <fpaolo@ucsd.edu>
+Dec 1, 2013
 """
 
 import os
@@ -23,7 +32,9 @@ import re
 import numpy as np
 import pandas as pd
 import altimpy as ap
+import statsmodels.api as sm
 from scipy import signal
+from glob import glob
 
 def window(x):
     return x * signal.windows.hanning(len(x))
@@ -33,8 +44,15 @@ def normalize(x):
     x /= x.std()   # normalize (std = 1)
     return x
 
+def hpfilter(y, lamb=7):
+    return sm.tsa.filters.hpfilter(y, lamb=lamb)[1]
+
+def gradient(y, dt=.25):
+    return np.gradient(y.values, dt)
+
 
 files = sys.argv[1:]
+#files = glob('/Users/fpaolo/data/shelves/all/*.csv')
 files.sort(key=ap.human_order)
 
 # take root of file name (no numbers)
@@ -43,14 +61,31 @@ fname1 = root + '_mat.txt'
 fname2 = root + '_vec.txt'
 
 # matrix file
+#------------
+'''
+THIS PROVIDES THE BEST RESUT FOR MSSA (with full seasonal data)
+Using the following parameters:
+sampling: 0.25, Varimax: yes, PCA channels: 10, window: 26, components: 10
+Obs: do not detrend!
+'''
 d = pd.concat([pd.read_csv(f, usecols=[0]) for f in files], axis=1).dropna(1, 'all')
+#d = d.apply(hpfilter)
+#d = d.apply(gradient)
 d = d.apply(normalize)
 d.to_csv(fname1, sep=' ', header=False, index=False, float_format='%.6f')
+#d.to_csv(root+'_grad_mat.txt', sep=' ', header=False, index=False, float_format='%.6f')
+'''
+d = d.mean(axis=1)
+d = normalize(d)     
+d.to_csv(root+'_mean_vec.txt', sep=' ', header=False, index=False, float_format='%.6f')
+'''
 
 # vector file
-#d = d.apply(signal.detrend)
-d = d.apply(window)
-d = d.unstack()
+#------------
+d = d.apply(window)  # window each time series
+d = d.unstack()      # concatenate all time series
+d = normalize(d)     # normalize concatenated series
+#d = pd.Series(signal.detrend(d))
 d.to_csv(fname2, header=False, index=False, float_format='%.6f')
 
 print 'stacked files ->', fname1, fname2
--- build/lib/altimpy/viz.py
+++ build/lib/altimpy/viz.py
@@ -664,7 +664,71 @@ def intitle(title='', loc=1, size=None, ax=None, **kwargs):
     at.patch.set_alpha(0.5)
     return ax
 
+"""
+Draws Hinton diagrams using matplotlib ( http://matplotlib.sf.net/ ).
+Hinton diagrams are a handy way of visualizing weight matrices, using
+colour to denote sign and area to denote magnitude.
+ 
+By David Warde-Farley -- user AT cs dot toronto dot edu (user = dwf)
+  with thanks to Geoffrey Hinton for providing the MATLAB code off of 
+  which this is modeled.
+ 
+Redistributable under the terms of the 3-clause BSD license 
+(see http://www.opensource.org/licenses/bsd-license.php for details)
+"""
+ 
+def _blob(x, y, area, colour):
+    """
+    Draws a square-shaped blob with the given area (< 1) at
+    the given coordinates.
+    """
+    hs = np.sqrt(area) / 2
+    xcorners = np.array([x - hs, x + hs, x + hs, x - hs])
+    ycorners = np.array([y - hs, y - hs, y + hs, y + hs])
+    plt.fill(xcorners, ycorners, colour, edgecolor=colour)
+ 
+def hinton(W, maxweight=None):
+    """
+    Draws a Hinton diagram for visualizing a weight matrix. 
+    Temporarily disables matplotlib interactive mode if it is on, 
+    otherwise this takes forever.
+    """
+    reenable = False
+    if plt.isinteractive():
+        plt.ioff()
+    
+    plt.clf()
+    height, width = W.shape
+    if not maxweight:
+        maxweight = 2**np.ceil(np.log(np.max(np.abs(W)))/np.log(2))
+        
+    plt.fill(np.array([0, width, width, 0]), 
+             np.array([0, 0, height, height]),
+             'gray')
+    
+    plt.axis('off')
+    plt.axis('equal')
+    for x in xrange(width):
+        for y in xrange(height):
+            _x = x+1
+            _y = y+1
+            w = W[y, x]
+            if w > 0:
+                _blob(_x - 0.5,
+                      height - _y + 0.5,
+                      min(1, w/maxweight),
+                      'white')
+            elif w < 0:
+                _blob(_x - 0.5,
+                      height - _y + 0.5, 
+                      min(1, -w/maxweight), 
+                      'black')
+    if reenable:
+        plt.ion()
 
+
+# DEPRECATED
+'''
 def hinton(W, max_weight=None, ax=None):
     """Draws a Hinton diagram for visualizing a weight matrix."""
     from matplotlib.patches import Rectangle
@@ -689,6 +753,7 @@ def hinton(W, max_weight=None, ax=None):
     ax.autoscale_view()
     # Reverse the yaxis limits
     ax.set_ylim(*ax.get_ylim()[::-1])
+''' 
 
 
 def plot_matrix(mat, title='', loc=1, plot=None, **kw):
@@ -704,8 +769,8 @@ def plot_matrix(mat, title='', loc=1, plot=None, **kw):
         ax2 = fig.add_subplot(122)
         ax1.matshow(mat)
         hinton(mat, ax=ax2)
-        t = add_inner_title(ax1, title, loc=loc)
-        t = add_inner_title(ax2, title, loc=loc)
+        t = intitle(title, loc=loc, ax=ax1)
+        t = intitle(title, loc=loc, ax=ax2)
     else:
         fig = plt.figure(figsize=(4,4))
         ax = fig.add_subplot((111))
@@ -719,7 +784,7 @@ def plot_matrix(mat, title='', loc=1, plot=None, **kw):
             ax.spy(mat, precision=0.1, markersize=6, **kw)
         else:
             raise ValueError('wrong argument `plot=%s`' % plot)
-        t = add_inner_title(ax, title, loc=loc)
+        t = intitle(title, loc=loc, ax=ax)
     return fig
 
 
