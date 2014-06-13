6dcebb274875655c2ece34e4067c5b39294a04f0
Fernando Paolo <fspaolo@gmail.com>
2014-06-12 19:16:25 -0700

    updated get_area_cells()

--- altimpy/tseries.py
+++ altimpy/tseries.py
@@ -652,7 +652,7 @@ def get_area_cells(grid, x, y):
 
     Notes
     -----
-    If coordinates are lon/lat, then area is given in m**2.
+    The area is given in m**2.
 
     """
     ny, nx = grid.shape
