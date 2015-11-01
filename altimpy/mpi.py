213b7b03e46f50506bc4830880cd4b6f12272245
Fernando Paolo <fspaolo@gmail.com>
2015-11-01 14:54:18 -0800

    added docstring to mpi.py module

---
+++ altimpy/mpi.py
@@ -0,0 +1,5 @@
+print """Note: for practical purposes this module has been separated
+         from this library. It is now a module in itself usable independently
+         from this package. You can check it out here:
+         https://github.com/fspaolo/mpisubmit
+      """
